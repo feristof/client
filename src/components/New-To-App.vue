03d3f7c61e3fa9366747874c1c7d3e4523a45921
Rish <patelr25@newpaltz.edu>
2022-11-22 18:43:55 -0500

    guide page - builing in progress

---
+++ src/components/New-To-App.vue
@@ -0,0 +1,13 @@
+<template>
+
+//creating guide for new users to get hands on with the app
+
+    //Ordered list to cover(summary of whole fitness app)
+    //Introduce the app
+    //Checkpoints to visit
+    //Show/describe a quick blueprint of the app - links to re-direct if possible
+    //Show the features of the app - custom timer on home page
+    //Show the benefits of the app
+
+
+</template>
